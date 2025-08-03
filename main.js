21c7b5512feadf5210ef280bfbb940d70c4f88cc
para0416 <parasama0416@gmail.com>
2025-08-03 17:54:24 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const config = {
     type: Phaser.AUTO,
     width: window.innerWidth,
     height: window.innerHeight,
+    backgroundColor: '#ffffff',
     physics: {
         default: 'arcade',
         arcade: {
@@ -22,42 +23,40 @@ let jumpPower = 0;
 const MAX_JUMP_POWER = 600;
 let platforms;
 let direction = 0;
-let background;
 let jumpStartTime = 0;
 
 const game = new Phaser.Game(config);
 
 function preload() {
-    this.load.image('background', 'background.png');
-    this.load.image('idle', 'https://labs.phaser.io/assets/sprites/phaser-dude.png');
-    this.load.image('charge', 'https://labs.phaser.io/assets/sprites/phaser-dude.png');
-    this.load.image('jump', 'https://labs.phaser.io/assets/sprites/phaser-dude.png');
-    this.load.image('platform', 'https://labs.phaser.io/assets/sprites/platform.png');
+    this.load.image('midori_jump', 'midori_jump.png');
+    this.load.image('platform1', 'https://labs.phaser.io/assets/sprites/platform.png');
+    this.load.image('platform2', 'https://labs.phaser.io/assets/sprites/platform.png');
+    this.load.image('platform3', 'https://labs.phaser.io/assets/sprites/platform.png');
 }
 
 function create() {
-    background = this.add.tileSprite(0, 0, config.width, config.height, 'background').setOrigin(0).setScrollFactor(0);
-
     platforms = this.physics.add.staticGroup();
 
     const platformData = [
-        { x: 240, y: 700 },
-        { x: 280, y: 550 },
-        { x: 200, y: 400 },
-        { x: 320, y: 300 },
-        { x: 160, y: 200 },
-        { x: 240, y: 100 }
+        { x: 240, y: 700, scaleX: 1.2 },
+        { x: 320, y: 550, scaleX: 0.8 },
+        { x: 160, y: 420, scaleX: 1.0 },
+        { x: 280, y: 300, scaleX: 0.6 },
+        { x: 200, y: 200, scaleX: 1.4 },
+        { x: 260, y: 100, scaleX: 0.7 }
     ];
-    platformData.forEach(p => platforms.create(p.x, p.y, 'platform'));
+    platformData.forEach((p, i) => {
+        let plat = platforms.create(p.x, p.y, 'platform' + ((i % 3) + 1));
+        plat.setScale(p.scaleX, 1).refreshBody();
+    });
 
-    player = this.physics.add.sprite(config.width / 2, 600, 'idle');
+    player = this.physics.add.sprite(config.width / 2, 600, 'midori_jump');
     player.setCollideWorldBounds(true);
     this.physics.add.collider(player, platforms);
 
     this.cameras.main.startFollow(player);
     this.cameras.main.setLerp(0.1, 0.1);
 
-    // タッチ操作によるジャンプチャージ
     this.input.on('pointerdown', pointer => {
         isCharging = true;
         jumpPower = 0;
@@ -73,16 +72,13 @@ function create() {
             player.setVelocityX(direction * 200);
             setTimeout(() => player.setVelocityX(0), 100);
             isCharging = false;
-            player.setTexture('jump');
         }
     });
 }
 
 function update() {
-    background.tilePositionY = this.cameras.main.scrollY * 0.5;
-
     if (player.body.touching.down && !isCharging) {
-        player.setTexture('idle');
+        player.setTexture('midori_jump');
     }
 
     if (player.y > this.cameras.main.scrollY + config.height + 100) {
